c64d81b6d3ac11a2adcdf497f0d8aabdb9dbe294
francois_popieul <francois.popieul@hotmail.fr>
2025-06-23 22:25:53 +0200

    ajout d'événements pour optimiser l'affichage du menu burger et des liens de navigation, correction de coquilles

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-const button = document.querySelector('button');
-console.log(button);
-button.addEventListener('submit', (event) => {
+const button = document.getElementById('participate');
+button.addEventListener('click', (event) => {
   event.preventDefault();
+  alert("Les inscriptions sont closes pour le moment.\n\nVeuillez réessaayer plus tard.")
+});
+
+const burgerButton = document.getElementById('burger-button');
+const navLinks = document.querySelectorAll('.nav-link');
+navLinks.forEach(navLink => {
+  navLink.addEventListener('click', () => {
+    if (window.getComputedStyle(burgerButton).display !== 'none') {
+      burgerButton.click();
+    }
+  });
+});
+
+
+const navbarCollapse = document.getElementById('navbarLinks');
+window.addEventListener('resize', () => {
+  if (window.getComputedStyle(navbarCollapse).display !== 'none') {
+      burgerButton.click();
+    }
 });
